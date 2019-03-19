498ca87958bedb11246ff14e67beb215ef35c3b3
Maulik Kanani <maulikkanani1894@gmail.com>
2019-03-19 14:38:13 -0400

    gitHub search initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
+
+class App extends Component {
+	state = {};
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">Hello React</div>
+			</div>
+		);
+	}
+}
+
+export default App;
